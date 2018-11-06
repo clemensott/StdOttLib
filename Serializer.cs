ea5da3df9ceef2484b6880ee93623cd8c86521a2
ShelbyBoss <42971547+ShelbyBoss@users.noreply.github.com>
2018-11-06 09:17:29 +0100

    Added Serializer

---
+++ Serializer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace StdOttWpfLib
+{
+    public class Serializer
+    {
+        public const char DefaultAddChar = '&';
+
+        public char AddChar { get; private set; }
+
+        public Serializer() : this(DefaultAddChar)
+        {
+        }
+
+        public Serializer(char addChar)
+        {
+            AddChar = addChar;
+        }
+
+        public IEnumerable<string> Split(StringBuilder dataString, char seperator)
+        {
+            return Split(dataString, seperator, AddChar);
+        }
+
+        public static IEnumerable<string> Split(StringBuilder dataString, char seperator, char addChar = DefaultAddChar)
+        {
+            while (dataString.Length > 0)
+            {
+                yield return GetUntil(ref dataString, seperator, addChar);
+            }
+        }
+
+        public string GetUntil(ref StringBuilder text, char seperator)
+        {
+            return GetUntil(ref text, seperator, AddChar);
+        }
+
+        public static string GetUntil(ref StringBuilder text, char seperator, char addChar = DefaultAddChar)
+        {
+            int length;
+            StringBuilder part = new StringBuilder(text.Length);
+
+            for (length = 0; length < text.Length; length++)
+            {
+                char c = text[length];
+
+                if (text[length] == seperator)
+                {
+                    length++;
+
+                    if (length >= text.Length || text[length] != addChar) break;
+                }
+
+                part.Append(c);
+            }
+
+            text.Remove(0, length);
+
+            return part.ToString();
+        }
+    }
+}
