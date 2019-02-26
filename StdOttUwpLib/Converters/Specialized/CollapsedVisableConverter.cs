dd1747e8d203b8603ac5d0cbdb97f455413c233b
ShelbyBoss <42971547+ShelbyBoss@users.noreply.github.com>
2019-02-26 21:51:32 +0100

    Added specialized converter

---
+++ StdOttUwpLib/Converters/Specialized/CollapsedVisableConverter.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml;
+
+namespace StdOttUwp.Converters.Specialized
+{
+    public class CollapsedVisableConverter : IsTrueToValueConverter
+    {
+        public CollapsedVisableConverter()
+        {
+            EqualsValue = Visibility.Collapsed;
+            NotEqualsValue = Visibility.Visible;
+        }
+    }
+}
---
+++ StdOttUwpLib/Converters/Specialized/VisableCollapsedConverter.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml;
+
+namespace StdOttUwp.Converters.Specialized
+{
+    public class VisableCollapsedConverter : IsTrueToValueConverter
+    {
+        public VisableCollapsedConverter()
+        {
+            EqualsValue = Visibility.Visible;
+            NotEqualsValue = Visibility.Collapsed;
+        }
+    }
+}
