349ea0e7442669234ebfe9f2de3afcb14f79bc35
Clemens Ott <clemens.ott@edu.fh-joanneum.at>
2019-06-15 12:01:31 +0200

    added RefValue

---
+++ StdOttStandardLib/RefValue.cs
@@ -0,0 +1,21 @@
+﻿namespace StdOttStandard
+{
+    public class RefValue<T>
+    {
+        public T Value { get; set; }
+
+        public RefValue()
+        {
+        }
+
+        public RefValue(T value)
+        {
+            Value = value;
+        }
+
+        public static implicit operator T(RefValue<T> obj)
+        {
+            return obj.Value;
+        }
+    }
+}
