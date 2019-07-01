627f908fefafa387fe3da4c6d638df22614afaee
Clemens Ott <clemens.ott@edu.fh-joanneum.at>
2019-07-01 19:45:37 +0200

    added ColorToSolidColorBrushConverter

---
+++ StdOttUwpLib/Converters/ColorToSolidColorBrushConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace StdOttUwp.Converters
+{
+    class ColorToSolidColorBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return new SolidColorBrush((Color)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return ((SolidColorBrush)value).Color;
+        }
+    }
+}
