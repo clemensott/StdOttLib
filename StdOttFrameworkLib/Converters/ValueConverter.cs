7fc01521975ed3b53b41fcad1d4deedd54a9a011
Clemens Ott <clemens.ott@outlook.de>
2019-09-14 20:42:26 +0200

    added ValueConverter with Event

---
+++ StdOttFrameworkLib/Converters/ValueConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace StdOttFramework.Converters
+{
+    public delegate object ConvertEventHandler(object value, Type targetType, object parameter, CultureInfo culture);
+    public delegate object ConvertBackEventHandler(object value, Type targetType, object parameter, CultureInfo culture);
+
+    public class ValueConverter : IValueConverter
+    {
+        public event ConvertEventHandler ConvertEvent;
+        public event ConvertBackEventHandler ConvertEventHandler;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ConvertEvent(value, targetType, parameter, culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ConvertBack(value, targetType, parameter, culture);
+        }
+    }
+}
