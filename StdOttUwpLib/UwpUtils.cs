cdb9cefcadd6a222f50ec439926e96f7c75764b3
Clemens <clemens.ott@outlook.de>
2019-12-16 19:33:43 +0100

    added SimpleDialogs

---
+++ StdOttUwpLib/UwpUtils.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace StdOttUwp
+{
+    public static class UwpUtils
+    {
+        public static async Task<bool> DialogBinary(string message, string title, string defaultOptionText, string cancelOptionText)
+        {
+            MessageDialog dialog = new MessageDialog(message, title);
+            IUICommand cmdDefault = new UICommand(defaultOptionText);
+            IUICommand cmdCancel = new UICommand(cancelOptionText);
+            dialog.Commands.Add(cmdDefault);
+            dialog.Commands.Add(cmdCancel);
+            dialog.DefaultCommandIndex = 0;
+            dialog.CancelCommandIndex = 1;
+
+            IUICommand result = await dialog.ShowAsync();
+
+            return result == cmdDefault;
+        }
+
+        public static async Task<bool?> DialogThreestate(string message, string title, string defaultOptionText, string cancelOptionText, string thirdOptionText)
+        {
+            MessageDialog dialog = new MessageDialog(message, title);
+            IUICommand cmdDefault = new UICommand(defaultOptionText);
+            IUICommand cmdSecond = new UICommand(cancelOptionText);
+            IUICommand cmdThird = new UICommand(thirdOptionText);
+            dialog.Commands.Add(cmdDefault);
+            dialog.Commands.Add(cmdSecond);
+            dialog.Commands.Add(cmdThird);
+            dialog.DefaultCommandIndex = 0;
+            dialog.CancelCommandIndex = 1;
+
+            IUICommand result = await dialog.ShowAsync();
+
+            if (result == cmdDefault) return true;
+            if (result == cmdSecond) return false;
+            return null;
+        }
+    }
+}
