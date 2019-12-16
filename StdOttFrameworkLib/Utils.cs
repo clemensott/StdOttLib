92d99b7b7459c6a6766ee436aaa84b70eb8cd654
Clemens <clemens.ott@outlook.de>
2019-12-16 19:33:21 +0100

    added crop methods

--- StdOttFrameworkLib/Utils.cs
+++ StdOttFrameworkLib/Utils.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 
 namespace StdOttFramework
@@ -19,6 +21,24 @@ namespace StdOttFramework
         public static string GetFullPath(string fileName)
         {
             return Path.Combine(Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]), fileName);
+        }
+
+        public static CroppedBitmap CropImage(BitmapSource bmp, double ratio)
+        {
+            Int32Rect rect;
+
+            if (bmp.PixelWidth < bmp.PixelHeight * ratio)
+            {
+                int height = (int)(bmp.PixelWidth / ratio);
+                rect = new Int32Rect(0, (int)(bmp.PixelHeight - height) / 2, bmp.PixelWidth, height);
+            }
+            else
+            {
+                int width = (int)(bmp.PixelHeight * ratio);
+                rect = new Int32Rect((int)(bmp.PixelWidth - width) / 2, 0, width, bmp.PixelHeight);
+            }
+
+            return new CroppedBitmap(bmp, rect);
         }
     }
 }
