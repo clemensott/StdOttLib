62b8625d73bdcf71bd3015b8e246d6ba568c80d6
Clemens <clemens.ott@outlook.de>
2020-01-07 09:09:17 +0100

    removes AppDataContainerList and added IconButton

--- StdOttUwpLib/ApplicationDataObjects/AppDataContainerObject.cs
+++ StdOttUwpLib/ApplicationDataObjects/AppDataContainerObject.cs
@@ -1,142 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using Windows.Foundation.Collections;
 using Windows.Storage;
 
 namespace StdOttUwp.ApplicationDataObjects
 {
     public class AppDataContainerObject : INotifyPropertyChanged
     {
-        private IPropertySet values;
-        protected readonly IDictionary<string, AppDataContainerObject> objects;
+        public ApplicationDataContainer Container { get; set; }
 
-        public ApplicationDataContainer Container { get; private set; }
-
-        public AppDataContainerObject()
+        public AppDataContainerObject(ApplicationDataContainer container)
         {
-            values = new PropertySet();
-            objects = new Dictionary<string, AppDataContainerObject>();
+            Container = container ?? throw new ArgumentNullException(nameof(container));
         }
 
-        public void SetContainer(ApplicationDataContainer container, bool copyToNew)
+        protected bool TryGetValue<T>(string propertyName, out T value)
         {
-            if (container == Container) return;
-            if (container == null)
+            try
             {
-                RemoveContainer();
-                return;
-            }
+                object objValue;
+                bool hasValue = Container.Values.TryGetValue(propertyName, out objValue) && objValue is T;
 
-            if (copyToNew && Container != null)
-            {
-                foreach (KeyValuePair<string, object> pair in Container.Values)
+                if (hasValue)
                 {
-                    container.Values[pair.Key] = pair.Value;
+                    value = (T)objValue;
+                    return true;
                 }
 
-                foreach (string removeKey in container.Values.Keys.Except(container.Values.Keys))
-                {
-                    container.Values.Remove(removeKey);
-                }
-
-                //foreach ((string key, ApplicationDataContainer value) in container.Containers.ToTuples())
-                //{
-                //    objects[key]
-                //    obj.SetContainer(true)
-                //}
+                value = default(T);
+                return false;
             }
-
-            Container = container;
-            values = null;
-        }
-
-        public void RemoveContainer()
-        {
-            values = new PropertySet();
-
-            foreach (KeyValuePair<string, object> pair in Container.Values)
+            catch
             {
-                values.Add(pair.Key, pair.Value);
-            }
-
-            foreach (AppDataContainerObject obj in objects.Values)
-            {
-                obj.RemoveContainer();
+                value = default(T);
+                return false;
             }
         }
 
-
-        protected T GetValue<T>(string propertyName)
+        protected T GetValue<T>(string propertyName, T defaultValue = default(T))
         {
-            object value;
-            return GetValues().TryGetValue(propertyName, out value) ? (T)value : default(T);
+            T value;
+            return TryGetValue(propertyName, out value) ? value : defaultValue;
         }
 
-        protected void SetValue(string propertyName, object value)
+        protected bool SetValue(string propertyName, object value)
         {
-            IDictionary<string, object> currentValues = GetValues();
-            if (currentValues.ContainsKey(propertyName)) currentValues[propertyName] = value;
-            else currentValues.Add(propertyName, value);
-
-            OnPropertyChanged(propertyName);
-        }
-
-        protected IDictionary<string, object> GetValues()
-        {
-            return Container?.Values ?? values;
-        }
-
-        protected T GetObject<T>(string propertyName) where T : AppDataContainerObject, new()
-        {
-            return GetObject<T>(propertyName, container =>
+            try
             {
-                T newObj = new T();
-                newObj.SetContainer(container, false);
-                return newObj;
-            });
-        }
+                IDictionary<string, object> currentValues = Container.Values;
+                if (currentValues.ContainsKey(propertyName)) currentValues[propertyName] = value;
+                else currentValues.Add(propertyName, value);
 
-        protected T GetObject<T>(string propertyName, Func<ApplicationDataContainer, T> create) where T : AppDataContainerObject
-        {
-            if (objects.ContainsKey(propertyName) && Container?.Containers.ContainsKey(propertyName) != false)
-            {
-                return (T)objects[propertyName];
+                OnPropertyChanged(propertyName);
+                return true;
             }
-
-            if (!objects.ContainsKey(propertyName) && Container?.Containers.ContainsKey(propertyName) != true)
+            catch
             {
-                return null;
+                return false;
             }
-
-            if (objects.ContainsKey(propertyName))
-            {
-                objects.Remove(propertyName);
-                return null;
-            }
-
-            ApplicationDataContainer container = Container.Containers[propertyName];
-            T newObj = create(container);
-
-            objects.Add(propertyName, newObj);
-            return newObj;
-        }
-
-        protected void SetObject<T>(string propertyName, T obj) where T : AppDataContainerObject
-        {
-            if (objects.ContainsKey(propertyName)) objects[propertyName] = obj;
-            else objects.Add(propertyName, obj);
-
-            if (Container == null) return;
-
-            ApplicationDataContainer container;
-            if (!Container.Containers.TryGetValue(propertyName, out container))
-            {
-                container = Container.CreateContainer(propertyName, ApplicationDataCreateDisposition.Always);
-            }
-
-            obj.SetContainer(container, true);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
---
+++ StdOttUwpLib/Controls/IconButton.cs
@@ -0,0 +1,35 @@
+﻿using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace StdOttUwp.Controls
+{
+    public class IconButton : Button
+    {
+        public static readonly DependencyProperty SymbolProperty = DependencyProperty.Register("Symbol", typeof(Symbol),
+            typeof(IconButton), new PropertyMetadata(null, OnSymbolPropertyChanged));
+
+        private static void OnSymbolPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            IconButton s = (IconButton)sender;
+            Symbol value = (Symbol)e.NewValue;
+
+            s.iconControl.Symbol = value;
+        }
+
+        private readonly SymbolIcon iconControl;
+
+        public Symbol Symbol
+        {
+            get => (Symbol)GetValue(SymbolProperty);
+            set => SetValue(SymbolProperty, value);
+        }
+
+        public IconButton()
+        {
+            Content = iconControl = new SymbolIcon();
+            Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+        }
+    }
+}
