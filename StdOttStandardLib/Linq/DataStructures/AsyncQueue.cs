531dee3b7c30ccda98f2a6012cdcb1189eeae6e4
ShelbyBoss <42971547+ShelbyBoss@users.noreply.github.com>
2020-04-22 18:25:52 +0200

    added AsyncQueue

---
+++ StdOttStandardLib/Linq/DataStructures/AsyncQueue.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StdOttStandard.Linq.DataStructures
+{
+    public class AsyncQueue<T>
+    {
+        private readonly SemaphoreSlim semSem, semQueue;
+        private readonly Queue<T> queue;
+
+        public bool IsEnd { get; private set; }
+
+        public int Count => queue.Count;
+
+        public AsyncQueue()
+        {
+            semSem = new SemaphoreSlim(1);
+            semQueue = new SemaphoreSlim(0);
+            queue = new Queue<T>();
+        }
+
+        public async Task End()
+        {
+            await semSem.WaitAsync();
+
+            if (!IsEnd)
+            {
+                IsEnd = true;
+                semQueue.Release();
+            }
+
+            semSem.Release();
+        }
+
+        public async Task Enqueue(T item)
+        {
+            await semSem.WaitAsync();
+
+            IsEnd = false;
+            queue.Enqueue(item);
+            if (semQueue.CurrentCount == 0) semQueue.Release();
+
+            semSem.Release();
+        }
+
+        public async Task Enqueue(IEnumerable<T> items)
+        {
+            await semSem.WaitAsync();
+
+            IsEnd = false;
+            foreach (T item in items) queue.Enqueue(item);
+            if (semQueue.CurrentCount == 0) semQueue.Release();
+
+            semSem.Release();
+        }
+
+        public async Task<(bool isEnd, T item)> Dequeue()
+        {
+            while (true)
+            {
+                try
+                {
+                    await semSem.WaitAsync();
+
+                    if (queue.Count > 0) return (false, queue.Dequeue());
+                    if (IsEnd) return (true, default(T));
+                }
+                finally
+                {
+                    semSem.Release();
+                }
+
+                await semQueue.WaitAsync();
+            }
+        }
+    }
+}
