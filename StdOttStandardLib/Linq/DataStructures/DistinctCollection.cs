21d61bf39f7e80028f77e26c363f4b8fe52d90ad
Clemens Ott <42971547+ShelbyBoss@users.noreply.github.com>
2020-09-08 12:06:26 +0200

    Moved DistinctCollection to data structures

--- StdOttStandardLib/Linq/DataStructures/DistinctCollection.cs
+++ StdOttStandardLib/Linq/DataStructures/DistinctCollection.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
-namespace StdOttStandard
+namespace StdOttStandard.Linq.DataStructures
 {
     public class DistinctCollection<T> : ObservableCollection<T>
     {
