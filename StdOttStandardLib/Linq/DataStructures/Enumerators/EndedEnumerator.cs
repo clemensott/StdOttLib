327a5864c40887d257ae877a24a688e52b349975
Clemens Ott <42971547+ShelbyBoss@users.noreply.github.com>
2020-09-14 10:31:38 +0200

    added EndedEnumerator

---
+++ StdOttStandardLib/Linq/DataStructures/Enumerators/EndedEnumerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace StdOttStandard.Linq.DataStructures.Enumerators
+{
+    public class EndedEnumerator<T> : IEnumerator<T>
+    {
+        private readonly IEnumerator<T> @base;
+
+        public T Current => @base.Current;
+
+        object IEnumerator.Current => ((IEnumerator)@base).Current;
+
+        public bool Ended { get; private set; }
+
+        public EndedEnumerator(IEnumerator<T> @base)
+        {
+            this.@base = @base ?? throw new ArgumentNullException(nameof(@base));
+            Ended = false;
+        }
+
+        public void Dispose()
+        {
+            @base.Dispose();
+        }
+
+        public bool MoveNext()
+        {
+            if (@base.MoveNext()) return true;
+
+            Ended = true;
+            return false;
+        }
+
+        public void Reset()
+        {
+            @base.Reset();
+        }
+    }
+}
