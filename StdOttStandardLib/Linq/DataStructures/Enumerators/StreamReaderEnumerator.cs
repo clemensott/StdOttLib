194eeecc137c4a2585cb6c7f8f2da78ec6cce3e7
Clemens Ott <42971547+ShelbyBoss@users.noreply.github.com>
2020-09-14 10:31:52 +0200

    added StreamReaderEnumerator

---
+++ StdOttStandardLib/Linq/DataStructures/Enumerators/StreamReaderEnumerator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace StdOttStandard.Linq.DataStructures.Enumerators
+{
+    class StreamReaderEnumerator : IEnumerator<string>
+    {
+        private readonly StreamReader reader;
+
+        public bool EndOfStream => reader.EndOfStream;
+
+        public string Current { get; private set; }
+
+        object IEnumerator.Current => Current;
+
+        public StreamReaderEnumerator(StreamReader reader)
+        {
+            this.reader = reader;
+        }
+
+        public bool MoveNext()
+        {
+            return ReadLine() != null;
+        }
+
+        public string ReadLine()
+        {
+            return Current = reader.ReadLine();
+        }
+
+        public async Task<bool> MoveNextAsync()
+        {
+            return (await ReadLineAsync()) != null;
+        }
+
+        public async Task<string> ReadLineAsync()
+        {
+            return Current = await reader.ReadLineAsync();
+        }
+
+        public void Reset()
+        {
+            throw new InvalidOperationException();
+        }
+
+        public void Dispose()
+        {
+            reader.Dispose();
+        }
+    }
+}
