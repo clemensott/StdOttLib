5e5b329eeab302ead9d74bed1d7c06d505f6a100
Clemens <clemens.ott@outlook.de>
2021-06-05 21:53:23 +0200

    added DeferralProvider

---
+++ StdOttUwpLib/DeferralProvider.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace StdOttStandard
+{
+    public class DeferralProvider
+    {
+        private int count;
+        private readonly TaskCompletionSource<bool> tcs;
+
+        public DeferralProvider()
+        {
+            count = 0;
+            tcs = new TaskCompletionSource<bool>();
+        }
+
+        public Deferral GetDeferral()
+        {
+            count++;
+            return new Deferral(() =>
+            {
+                count--;
+                if (count == 0) tcs.SetResult(true);
+            });
+        }
+
+        public async Task Await()
+        {
+            if (count > 0) await tcs.Task;
+            else if (!tcs.Task.IsCompleted) tcs.SetResult(true);
+        }
+    }
+}
